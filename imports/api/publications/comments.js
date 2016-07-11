5ada1cdd81940cab447c71daa45ec4ea1960177f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-07-11 17:37:21 +0100

    Update schema and publications

---
+++ imports/api/publications/comments.js
@@ -0,0 +1,8 @@
+import { Comments } from '../collections/comments.js';
+import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
+
+Meteor.publish('comments', (id) => {
+  check(id, Match.Any);
+  return Comments.find({ postId: id });
+});
