d72561c1d321dcbdf79aec069797e93af761b9eb
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-07-11 17:36:52 +0100

    Update server

--- server/main.js
+++ server/main.js
@@ -1,13 +1,18 @@
 /* eslint no-underscore-dangle: 0 */
 
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/db/schemas.js';
 import '../imports/api/publications/posts.js';
+import '../imports/api/publications/comments.js';
 import { Posts } from '../imports/api/collections/posts';
 import { Comments } from '../imports/api/collections/comments';
 import { Words } from '../imports/api/collections/words';
 import './seeds.js';
 
 Meteor.startup(() => {
+  // Posts.remove({});
+  // Comments.remove({});
+  // Words.remove({});
   if (Posts.find().count() === 0) {
     Meteor.call('Database.seed');
   }
@@ -17,6 +22,6 @@ Meteor.startup(() => {
   });
 
   Comments._ensureIndex({
-    post: 1,
+    postId: 1,
   });
 });
--- server/seeds.js
+++ server/seeds.js
@@ -5,7 +5,7 @@ import { Comments } from '../imports/api/collections/comments';
 import { Words } from '../imports/api/collections/words';
 
 import WordsSeed from '../imports/api/fixtures/words';
-import PostsSeed from '../imports/api/fixtures/posts';
+import Secrets from '../server/data.js';
 
 import { Meteor } from 'meteor/meteor';
 import { _ } from 'meteor/underscore';
@@ -18,15 +18,17 @@ Meteor.methods({
       });
     });
 
-    _.forEach(PostsSeed, (post) => {
-      Posts.insert(post, (_error, result) => {
-        const Comment = {
-          post: result,
-          ip: '127.0.0.1',
-          body: 'adventurous, awesome, cool',
-        };
-        Comments.insert(Comment, () => {
-          console.log('Comment added for', result);
+    _.forEach(Secrets, (post) => {
+      Posts.insert({ body: post.secret.body, ip: '127.0.0.1' }, (_error, result) => {
+        _.forEach(post.secret.comments, (comment) => {
+          const Comment = {
+            postId: result,
+            ip: '127.0.0.1',
+            body: comment.body,
+          };
+          Comments.insert(Comment, () => {
+            console.log('Comment added for', result);
+          });
         });
       });
     });
